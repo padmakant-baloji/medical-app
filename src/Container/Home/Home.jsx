7f0e8df0e4c03e59c624130203b8a0494c6f9cb1
padmakant <padmakant.pb@casaone.com>
2021-05-21 22:03:42 +0530

    Added Clinic name and logo

--- src/Container/Home/Home.jsx
+++ src/Container/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "../../Components/Header/Header";
 
 const Home = () => {
   console.log("Home");
-  return <div>Working.....</div>;
+  return (
+  <div>
+    <Header />
+    Working.....
+    </div>
+    );
 };
 
 export default Home;
